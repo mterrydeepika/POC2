5e451739a6add36bbfe68d98c9424ae1edd4bc5c
Terry Deepika <mterrydeepika@gmail.com>
2019-12-04 07:17:03 +0000

    1-Adding all files for springmvc

---
+++ src/com/sample/springmvc/dao/CustomerDAO.java
@@ -0,0 +1,19 @@
+package com.sample.springmvc.dao;
+
+import java.util.List;
+
+import com.sample.springmvc.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+
+	public List<Customer> searchCustomers(String theSearchName);
+	
+}
---
+++ src/com/sample/springmvc/service/CustomerService.java
@@ -0,0 +1,17 @@
+package com.sample.springmvc.service;
+
+import java.util.List;
+
+import com.sample.springmvc.entity.Customer;
+
+public interface CustomerService {
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+
+	public List<Customer> searchCustomers(String theSearchName);
+}
